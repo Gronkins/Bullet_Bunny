9edebddf4412cc77ad758b8c12b22f50453e69e8
Gronkins <raymond.connors0@gmail.com>
2023-04-25 19:33:33 +0800

    Unity Project

---
+++ Unity/Bullet Bunny/Assets/Scripts/Collectibles/Collectible.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    PlayerStats playerStats;
+    PlayerMovement playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerStats = GetComponent<PlayerStats>();
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.collider.tag == "Player")
+        {
+            Debug.Log("Is touching");
+            playerMovement.numOfDashes = playerMovement.maxDashes;
+            Destroy(gameObject);
+        }
+    }
+}
