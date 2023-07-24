39fa54f1cecdc2c582ffb0a3567ac004aa4f262f
Gronkins <raymond.connors0@gmail.com>
2023-07-24 20:56:53 +0800

    Working on Electric Floor Prefabs

---
+++ Unity/Bullet Bunny/Assets/Scripts/Electricity/ElecricityAnimatorController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElecricityAnimatorController : MonoBehaviour
+{
+    private Animator animator;
+    public ElectricityDirection electricityDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetAnimation();
+    }
+
+    private void SetAnimation()
+    {
+        switch (electricityDirection)
+        {
+            case ElectricityDirection.Up:
+                animator.SetBool("IsUp", true);
+                break;
+            case ElectricityDirection.Down:
+                animator.SetBool("IsDown", true);
+                break;
+            case ElectricityDirection.Left:
+                animator.SetBool("IsLeft", true);
+                break;
+            case ElectricityDirection.Right:
+                animator.SetBool("IsRight", true);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public enum ElectricityDirection
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
