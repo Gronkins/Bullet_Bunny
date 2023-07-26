c5be0024dbd7ee5a445eec1ea5536721b359a698
Gronkins <raymond.connors0@gmail.com>
2023-07-26 18:15:46 +0800

    Added Level Transition Prefab

--- Unity/Bullet Bunny/Assets/Scripts/Managers/LevelTransition.cs
+++ Unity/Bullet Bunny/Assets/Scripts/Managers/LevelTransition.cs
@@ -7,24 +7,43 @@ public class LevelTransition : MonoBehaviour
 {
     ScreenManager screenManager;
     TilemapRenderer tilemapRenderer;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         tilemapRenderer = GetComponent<TilemapRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         screenManager = FindObjectOfType<ScreenManager>();
-        tilemapRenderer.material.color = new Color(1f, 1f, 1f, 0f); //Hides the level transition tile in-game
+        HideSprite();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HideSprite()
     {
+        if (tilemapRenderer != null)
+        {
+            tilemapRenderer.material.color = new Color(1f, 1f, 1f, 0f); //Hides the level transition tile in-game
+        }
 
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.material.color = new Color(1f, 1f, 1f, 0f); //Hides the level transition tile in-game
+        }
+    
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Player")
+        {
+            Debug.Log("Is touching Transition");
+            screenManager.LoadNextScene();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.tag == "Player")
         {
             Debug.Log("Is touching Transition");
             screenManager.LoadNextScene();
