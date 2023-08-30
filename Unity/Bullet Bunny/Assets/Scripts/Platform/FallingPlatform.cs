826633db915454bf521eebe76ed6381c246676f9
Gronkins <raymond.connors0@gmail.com>
2023-08-30 17:31:57 +0800

    Falling Platform Adjustments

--- Unity/Bullet Bunny/Assets/Scripts/Platform/FallingPlatform.cs
+++ Unity/Bullet Bunny/Assets/Scripts/Platform/FallingPlatform.cs
@@ -7,6 +7,7 @@ public class FallingPlatform : MonoBehaviour
     private Animator animator;
     private Rigidbody2D rigidBody2D;
     private FallingPlatformController controller;
+    public float timeBeforeFall = 0.5f;
 
     private void Awake()
     {
@@ -20,22 +21,22 @@ public class FallingPlatform : MonoBehaviour
         if(collision.collider.tag == "Player")
         {
             Debug.Log("Player touched");
+            controller.SetFalling();
             StartCoroutine(StartFall());
         }
     }
 
     private IEnumerator StartFall()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(timeBeforeFall);
         animator.SetTrigger("Falling");
-        controller.SetFalling();
+        //controller.SetFalling();
         StartCoroutine(Fall());
     }
     
     private IEnumerator Fall()
     {
         yield return new WaitForSeconds(1);
-        //animator.SetTrigger("Falling");
         rigidBody2D.bodyType = RigidbodyType2D.Dynamic;
         rigidBody2D.mass = 1000f;
         rigidBody2D.gravityScale = 2f;
