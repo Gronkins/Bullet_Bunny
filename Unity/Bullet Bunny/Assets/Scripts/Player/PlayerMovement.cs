40682b0d25d8e5ee23bbaa161edcf964ad84e946
Gronkins <raymond.connors0@gmail.com>
2023-09-02 00:54:58 +0800

    Removed Ability To Downwards Diagonally Bullet Jump While Sliding

--- Unity/Bullet Bunny/Assets/Scripts/Player/PlayerMovement.cs
+++ Unity/Bullet Bunny/Assets/Scripts/Player/PlayerMovement.cs
@@ -300,7 +300,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isSliding)
         {
-            if(dashDirection == new Vector2(0, 0) || dashDirection == new Vector2(-1, 0) || dashDirection == new Vector2(1, 0) || (dashDirection == new Vector2(0, -1) && isGrounded))
+            if(dashDirection == new Vector2(0, 0) || dashDirection == new Vector2(-1, 0) || dashDirection == new Vector2(1, 0) || vertical == -1 || (dashDirection == new Vector2(0, -1) && isGrounded))
             {
                 return false;
             }
