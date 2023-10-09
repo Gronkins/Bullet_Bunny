96024ab19a80083ca07e25cfb38c35407a3df48e
Gronkins <raymond.connors0@gmail.com>
2023-10-09 18:08:49 +0800

    Update LevelScroll.cs

--- Unity/Bullet Bunny/Assets/Scripts/Menus/LevelScroll.cs
+++ Unity/Bullet Bunny/Assets/Scripts/Menus/LevelScroll.cs
@@ -7,8 +7,6 @@ public class LevelScroll: MonoBehaviour
 {
     public Transform level;
     public float scrollSpeed = 1.0f;
-    public float spawnThreshold = 10.0f; // Adjust this value based on your level's size
-
     private Transform duplicateLevel;
     private float levelWidth;
 
@@ -25,10 +23,8 @@ public class LevelScroll: MonoBehaviour
 
         if (level.position.x <= -levelWidth)
         {
-            // Move level to the right to prepare for duplication
             level.position += new Vector3(levelWidth, 0, 0);
             
-            // Spawn a duplicate of the level at the end
             Destroy(duplicateLevel.gameObject);
             duplicateLevel = Instantiate(level, level.position + new Vector3(levelWidth, 0, 0), Quaternion.identity);
         }
