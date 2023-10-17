bf44bc179b44ed6bc0fe93c559ea1104b79c7453
Gronkins <raymond.connors0@gmail.com>
2023-10-17 21:54:01 +0800

    Implementing Checkpoint Sprites And Animations

--- Unity/Bullet Bunny/Assets/Scripts/Objects/Checkpoint.cs
+++ Unity/Bullet Bunny/Assets/Scripts/Objects/Checkpoint.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if (GameManager.Instance.hasCheckpoint)
+        {
+            animator.SetBool("IsActive", true);
+        }
+    }
+    
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
         if (collider2D.tag == "Player")
         {
             GameManager.Instance.SetCheckpoint(gameObject.transform.position);
+            animator.SetTrigger("Activate");
             Debug.Log("Player has checkpoint!");
         }
     }
